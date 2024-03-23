6a7ec85cc928c5f0ad9bbf5b2e7b5e01a03735d9
Akash Sharma <87300147+ak8459@users.noreply.github.com>
2024-03-23 22:54:51 +0530

    updated bug

--- Server/middleware/auth.middleware.js
+++ Server/middleware/auth.middleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const authenticateUser = (req, res, next) => {
     const authCookie = req.cookies.token;
     try {
-        jwt.verify(authCookie, process.env.JWT_SECRET, (err, user) => {
+        jwt.verify(authCookie, 'aksdwdwed', (err, user) => {
             if (err) {
                 return res.status(401).send({
                     success: false,
