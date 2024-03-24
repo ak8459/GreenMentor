9cf43a4f6a1e9092d42186891990d81526308cc2
Akash Sharma <87300147+ak8459@users.noreply.github.com>
2024-03-24 13:05:40 +0530

    adad

--- Server/index.js
+++ Server/index.js
@@ -10,6 +10,12 @@ require('dotenv').config()
 
 let port = process.env.PORT
 const app = express();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,  Accept, x - client - key, x - client - token, x - client - secret, Authorization")
+    next()
+})
 
 app.use(express.json());
 app.use(cors({
