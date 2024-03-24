7542960f523d7339b44e525236fd962352c51f11
Akash Sharma <87300147+ak8459@users.noreply.github.com>
2024-03-24 14:18:29 +0530

    adas

--- Server/index.js
+++ Server/index.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.options('*', cors());
 app.use(cors({
     origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
